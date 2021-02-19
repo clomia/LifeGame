6586461f12dcae4b0ea6e15552366ca07f686be6
cloe001000 <69475099+cloe001000@users.noreply.github.com>
2021-02-20 07:16:32 +0900

    그리드 만들어보기

--- experimental.py
+++ experimental.py
@@ -2,20 +2,26 @@ from ursina import *
 
 app = Ursina()
 
-
 # camera.x = 10
+def screen_control():
+    camera.x += held_keys["d"] * time.dt * 10
+    camera.x -= held_keys["a"] * time.dt * 10
+    camera.y += held_keys["w"] * time.dt * 10
+    camera.y -= held_keys["s"] * time.dt * 10
 
 
-class Cell(Button):
+class CellGrid(Entity):
     def __init__(self, **kwargs):
         super().__init__()
-        self.model = Grid(10, 10)
-        self.scale = Vec2(0.7, 0.7)
+        self.model = Grid(1000, 1000)
+        self.scale = Vec2(500, 500)
+        self.color = color.rgba(255, 255, 255, a=15)
 
     def update(self):
-        print(mouse.point)
+        screen_control()
 
 
-Cell()
+EditorCamera(zoom_speed=2)
+CellGrid()
 
 app.run()
