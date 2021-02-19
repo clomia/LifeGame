193ee3fb001f489428bf10971a3096b2b3903756
cloe001000 <69475099+cloe001000@users.noreply.github.com>
2021-02-20 05:03:32 +0900

    README.md

--- experimental.py
+++ experimental.py
@@ -2,6 +2,20 @@ from ursina import *
 
 app = Ursina()
 
-Entity(model=Grid(10, 10), scale=Vec2(7, 7))
+
+# camera.x = 10
+
+
+class Cell(Button):
+    def __init__(self, **kwargs):
+        super().__init__()
+        self.model = Grid(10, 10)
+        self.scale = Vec2(0.7, 0.7)
+
+    def update(self):
+        print(mouse.point)
+
+
+Cell()
 
 app.run()
--- test_2D.py
+++ test_2D.py
@@ -1,4 +1,14 @@
 from ursina import *
 
 app = Ursina()
+
+Button(parent=scene, text="a")
+
+
+def update():
+    print(mouse.position, mouse.point)
+
+
+Cursor()
+mouse.visible = False
 app.run()
