d8a48283b6f14216b1b136b528d3882cdbba46db
cloe001000 <69475099+cloe001000@users.noreply.github.com>
2021-03-17 20:46:44 +0900

    그리드 범위 벗어나는 좌표 연산 추가

--- experimental.py
+++ experimental.py
@@ -5,9 +5,10 @@ app = Ursina()
 EditorCamera(zoom_speed=2)
 
 
-class Cell(Entity):
-    def __init__(self, co):
+class Cell(Entity):  # todo 코드 다시짤때는 grid를 parent로?
+    def __init__(self, grid, co):
         super().__init__()
+        self.grid = grid
         self.model = "sphere"
         self.model.colorize(
             down=color.magenta,
@@ -21,6 +22,7 @@ class Cell(Entity):
         self.scale = (0.5, 0.5, 0.5)
         self.co = co
         x_coordinates, y_coordinates = co
+        # * 0.5는 cell크기이다 점에서 면으로 보정하는 값은 그 절반인 0.25이다
         self.x = 0.25 + (0.5 * x_coordinates)
         self.y = 0.25 + (0.5 * y_coordinates)
         self.z = 0.375
@@ -35,10 +37,11 @@ class Cell(Entity):
 class CellGrid(Entity):
     def __init__(self, scale: int):
         super().__init__()
+        self.scale_value = scale
         self.model = Grid(scale, scale)
         self.scale = Vec2(scale // 2, scale // 2)
         self.color = color.rgba(255, 255, 255, a=15)
-        self.cell_map = {}
+        self.cell_map = {}  #! 커스텀 딕셔너리로 해서 스스로 cell을 렌더링 하도록 하자
         self.negative_range = -scale // 2
         self.positive_range = (scale // 2) - 1
         grid_range = range(-scale // 2, scale // 2)
@@ -46,23 +49,32 @@ class CellGrid(Entity):
             for y in grid_range:
                 self.cell_map[(x, y)] = False
 
+    def co_calculation(self, co: int) -> int:
+        """ grid를 벗어나는 좌표를 grid내부좌표로 변환한다"""
+
+        def calculation(value):
+            ne_threshold = self.negative_range
+            po_threshold = self.positive_range
+            grid_scale = self.scale_value
+            if value < ne_threshold:
+                value = po_threshold - (-value + po_threshold) % grid_scale
+            if value > po_threshold:
+                value = ne_threshold + (value + -ne_threshold) % grid_scale
+            return value
+
+        x, y = co
+        return (calculation(x), calculation(y))
+
     def check(self, co):
         """ cell이 있으면 cell을 반환한다. 없다면 False를 반환한다"""
-        x, y = co
-        if (
-            x < self.negative_range
-            or x > self.positive_range
-            or y < self.negative_range
-            or y > self.positive_range
-        ):
-            return False
+        co = self.co_calculation(co)
         target = self.cell_map[co]
         if (not target) and target != False:
             return True
         else:
             return target
 
-    def screen_control(self):
+    def screen_control(self):  # todo 롤처럼 마우스가 끝에 닿는걸로
         camera.x += held_keys["d"] * time.dt * 10
         camera.x -= held_keys["a"] * time.dt * 10
         camera.y += held_keys["w"] * time.dt * 10
@@ -98,22 +110,46 @@ class CellGrid(Entity):
                 destroy(cell)
                 return (cell.co, False)
         else:
+            co = self.co_calculation(co)
             neighbor_count = self.neighbor_counter(co=co)
             if neighbor_count == 3:
-                return (co, Cell(co))
+                return (co, Cell(self, co))
 
     def init(self, co_list):
         for co in co_list:
-            self.cell_map[co] = Cell(co)
+            co = self.co_calculation(co)
+            self.cell_map[co] = Cell(self, co)
 
 
 grid = CellGrid(50)
-grid.init([(-1, 1), (-1, 0), (0, 1), (0, -1), (1, 1)])
+glider = [(-1, 1), (-1, 0), (0, 1), (0, -1), (1, 1)]
+infinity = [
+    (0, 0),
+    (1, 1),
+    (2, 0),
+    (2, 1),
+    (3, 2),
+    (4, 0),
+    (4, 1),
+    (4, 2),
+    (0, 3),
+    (0, 4),
+    (1, 4),
+    (2, 4),
+    (4, 4),
+]
+
+grid.init(infinity)
 
 
 def input(key):
-    if key == "left mouse down":
+    if key == "space":
         grid.execute_logic()
 
+
+"""
+def update():
+    grid.execute_logic()
+"""
 
 app.run()
