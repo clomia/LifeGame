9db53b394ea045e6c6c8388f373e1fe7fd74a248
cloe001000 <69475099+cloe001000@users.noreply.github.com>
2021-04-06 10:39:08 +0900

    [simul 프로세스][CacheManagementPipe]데이터 과부화(연산 프로세스의 데이터 제공) 방지

--- client/core/simul_proc.py
+++ client/core/simul_proc.py
@@ -3,10 +3,17 @@ from queue import Queue
 from artifacts import *
 from scripts import *
 
-# 이 pipe_queue로 들어온건 소비자 pipe로 분해되서 바로 들어간다
+
 class CacheManagementPipe(Queue):
     def __init__(self, limit: int):
-        """ pipe를 통과하는 데이터들을 모니터링 하면서 정보량을 관리"""
+        """
+        이동하는 데이터가 limit를 넘어가면 데이터 수신 간격을 대폭 늘립니다.
+
+        메인 프로세스의 연산 쓰레드가 제공한 데이터가 통째로 이 파이프를 통과합니다.
+        다음 파이프에서 데이터가 분해되며 그 다음에는 필요할때에 컨테이너로 연결됩니다.
+
+        하단 파이프와 컨테이너에서 받는 부담을 이곳에서 관리하기 위한 클래스입니다.
+        """
         super().__init__()
         self.limit = limit
         self.cache_full = lambda: self.cache_counter >= limit
