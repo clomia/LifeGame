22bd8db011f1b46aa1aaa3d0edade34207e1af80
cloe001000 <69475099+cloe001000@users.noreply.github.com>
2021-04-13 11:08:48 +0900

    CountDown Class Created

--- client/core/artifacts/origin/tools.py
+++ client/core/artifacts/origin/tools.py
@@ -10,25 +10,32 @@ outline = lambda obj, opacity: Entity(
 SIGNAL = object()
 
 
-class FullUI(Entity):
-    """ 화면을 가득 채우는 UI를 만들기 위한 부모클래스"""
+class UI(Entity):
+    """ 화면에 고정된 Entity(->UI)를 만들기 위한 부모클래스"""
+
+    Center = (0, 0)
+    Top = (0, 0.437)
+    Bottom = (0, -0.437)
+    Left = (-0.83, 0)
+    Right = (0.795, 0)
+    Top_Right = (0.795, 0.437)
+    Top_Left = (-0.83, 0.437)
+    Bottom_Right = (0.795, -0.437)
+    Bottom_Left = (-0.83, -0.437)
 
     def __init__(self):
         super().__init__()
         self.parent = camera.ui
         self.origin = (-0.5, 0.5)
+
+
+class FullUI(UI):
+    """ 화면을 가득 채우는 UI를 만들기 위한 부모클래스"""
+
+    def __init__(self):
+        super().__init__()
         self.model = "quad"
         x_ratio, y_ratio = window.screen_resolution
         value = 1 / y_ratio
         self.scale_x = x_ratio * value
         self.scale_y = y_ratio * value
-
-
-class UI(Entity):
-    """ 화면에 고정된 Entity(->UI)를 만들기 위한 부모클래스"""
-
-    def __init__(self):
-        super().__init__()
-        self.parent = camera.ui
-        self.origin = (-0.5, 0.5)
-        self.model = "quad"
--- client/core/artifacts/simul_ui.py
+++ client/core/artifacts/simul_ui.py
@@ -1,17 +1,48 @@
 """ 카운트다운, 점수판, 게임 로직등에 필요한 ui도구 모듈 | CellController클래스를 다루는 모듈이다."""
 from ursina import *
+from .origin import *
 
 
-class BasicText:
+class BasicText(UI):
     """ 간편 텍스트 컨트롤러 (정해진 위치,모양,색)"""
+
+    def __init__(self, string, *arg):
+        super().__init__()
+        Text(string)
 
 
 class BasicUI:
     """ 좌상단 점수판 등등"""
 
 
-class CountDown:
+class CountDown(UI):
     """ 게임 시작 후 카운트다운"""
+
+    def __init__(self, count: int = 10, pipe_func=lambda: print("함수가 실행됩니다")):
+        """ 카운트 다운 종료 후의 실행을 pipe_func로 전달해주세요"""
+        super().__init__()
+        self.text_size = 1.5
+        self.numbers = [str(count)]
+        while count:
+            count -= 1
+            self.numbers.append(str(count))
+        self.fade_duration = 0.4
+        self.action = Sequence(loop=False)
+        for text in self.fade_setting():
+            self.action.append(Func(text.fade_in, duration=self.fade_duration))
+            self.action.append(self.fade_duration)
+            self.action.append(1 - 2 * self.fade_duration)
+            self.action.append(Func(text.fade_out, duration=self.fade_duration))
+            self.action.append(self.fade_duration)
+            self.action.append(Func(destroy, text))
+        self.action.append(Func(pipe_func))
+        self.action.start()
+
+    def fade_setting(self):
+        for number in self.numbers:
+            text = Text(number, position=UI.Top, scale=self.text_size)
+            text.fade_out(duration=0)
+            yield text
 
 
 class IterStep:
@@ -20,11 +51,3 @@ class IterStep:
 
 def trigger(pipe_queue):
     """ 모든 클래스를 이어주는 편의 함수"""
-
-
-if __name__ == "__main__":
-    from origin import *
-
-    with simul():
-        BasicText("안녕")
-        trigger()
